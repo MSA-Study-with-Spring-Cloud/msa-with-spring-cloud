4005ce2837f91251a437bfffe11b7de0b01d20e4
prkim99@gmail.com <prkim99@gmail.com>
2023-06-14 04:13:10 +0900

    [section 1] Eureka Service Discovery - 프로젝트 생성

---
+++ discoveryservice/src/main/java/com/example/discoveryservice/DiscoveryserviceApplication.java
@@ -0,0 +1,15 @@
+package com.example.discoveryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer //서버의 자격으로 등록 해줌 Service Discovery로서 지금 프로젝트를 구동하겠다는 뜻
+public class DiscoveryserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryserviceApplication.class, args);
+    }
+
+}
