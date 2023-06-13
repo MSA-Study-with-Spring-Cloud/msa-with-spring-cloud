db0334a8ee68216cb851da88e3c08ec172594c3b
prkim99@gmail.com <prkim99@gmail.com>
2023-06-14 03:14:36 +0900

    [section 1] Spring Cloud Netflix Eureka

--- ecoomerce/src/main/java/com/example/ecoomerce/EcoomerceApplication.java
+++ ecoomerce/src/main/java/com/example/ecoomerce/EcoomerceApplication.java
@@ -2,8 +2,10 @@ package com.example.ecoomerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer //서버의 자격으로 등록 해줌 Service Discovery로서 지금 프로젝트를 구동하겠다는 뜻
 public class EcoomerceApplication {
 
     public static void main(String[] args) {
