6b18ad88330e7375c6b6863c520d616ba328ec8f
prkim99@gmail.com <prkim99@gmail.com>
2023-07-23 20:41:16 +0900

    [section 6] Users Microservice - JWT 처리 과정

--- user-service/src/main/java/com/example/userservice/security/AuthenticationFilter.java
+++ user-service/src/main/java/com/example/userservice/security/AuthenticationFilter.java
@@ -4,6 +4,8 @@ import com.example.userservice.dto.UserDto;
 import com.example.userservice.service.UserService;
 import com.example.userservice.vo.RequestLogin;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.env.Environment;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -19,6 +21,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Date;
 
 @Slf4j
 public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
@@ -65,6 +68,15 @@ public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
         //로그인시 다섯번째로 수행
 
         String userName= ((User)authResult.getPrincipal()).getUsername();
-        UserDto userDto=userService.getUserDetailsByEmail(userName);
+        UserDto userDetail=userService.getUserDetailsByEmail(userName);
+
+        String token=Jwts.builder()
+                .setSubject(userDetail.getUserId())
+                .setExpiration(new Date(System.currentTimeMillis()+Long.parseLong(env.getProperty("token.expiration_time"))))
+                .signWith(SignatureAlgorithm.ES512,env.getProperty("token.secret"))
+                .compact();
+
+        response.addHeader("token",token);;
+        response.addHeader("userId",userDetail.getUserId());
     }
 }
